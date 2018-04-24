1d70ca6b41dbe6e2e72c515b68eff99b0673e0a9
ColePresnell <cole.presnell@gmail.com>
2018-04-23 23:12:16 -0500

    Still making changes, but have most of the js done

---
+++ assets/game.js
@@ -0,0 +1,130 @@
+// Define variables
+
+
+
+
+
+//create target number ranging from 19-120
+
+
+var crystals = $("#crystals");
+
+var counter = 0;
+var targetNumber;
+
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+
+var crystalValue = 0;
+var wins = 0;
+var losses = 0;
+
+
+//create crystals with values ranging from 1-12, assigned randomly
+
+// var numberOptions = Math.ceil((Math.random() * 11) + 1);
+
+
+//For each random number chosen, we will create an imageCrystal
+
+// "New Game" button
+
+$(document).on("click", ".numButton", function () {
+    targetNumber = Math.floor((Math.random() * 100) + 19);
+// takes the random number and prints it to the html
+    $("#numberToGuess").text(targetNumber);
+
+});
+
+
+// Here we are taking a click on the first crystal and assigning it a random number between 1-12. We are taking the random number and assigning it to a separate variable. Next, we are removing the class that allows us to generate a random number - we want this crystal to only produce the same number until the end of the game. Finally, we are adding the integer result to the running total of the total crystalValue. 
+$(document).on("click", ".classOne", function () {
+    var numberOptions = Math.ceil((Math.random() * 11) + 1);
+    crystalOne = numberOptions;
+    $("#one").removeClass("classOne");
+    $("#one").addClass("newCrystalOne");
+    crystalValue += crystalOne;
+    console.log(crystalOne);
+    
+});
+
+// We have removed the ability to generate a random number on crystal one, but we still want to be able to click on it so that we can add the assigned value of the crystal to the running total.
+$(document).on("click", ".newCrystalOne", function () {
+    crystalValue += crystalOne;
+    console.log(crystalOne);
+    console.log(crystalValue);
+});
+
+//Doing the same random number function for crystal two that we did for the first crystal
+$(document).on("click", ".classTwo", function () {
+    var numberOptions = Math.ceil((Math.random() * 11) + 1);
+    crystalTwo = numberOptions;
+    $("#two").removeClass("classTwo");
+    $("#two").addClass("newCrystalTwo");
+    crystalValue += crystalTwo;
+    console.log(crystalTwo);
+    console.log(crystalValue);
+    
+    
+});
+
+// Allowing crystal two to add to the running total by adding the same number after the first click.
+$(document).on("click", ".newCrystalTwo", function () {
+    crystalValue += crystalTwo;
+    console.log(crystalTwo);
+    console.log(crystalValue);
+});
+
+//Doing the same random number function for crystal three that we did for the others
+$(document).on("click", ".classThree", function () {
+    var numberOptions = Math.ceil((Math.random() * 11) + 1);
+    crystalThree = numberOptions;
+    $("#three").removeClass("classThree");
+    $("#three").addClass("newCrystalThree");
+    crystalValue += crystalThree;
+    console.log(crystalThree);
+    console.log(crystalValue);
+    
+    
+});
+
+// Allowing crystal three to add to the running total by adding the same number after the first click.
+$(document).on("click", ".newCrystalThree", function () {
+    crystalValue += crystalThree;
+    console.log(crystalThree);
+    console.log(crystalValue);
+});
+
+//Doing the same random number function for crystal four that we did for the other
+$(document).on("click", ".classFour", function () {
+    var numberOptions = Math.ceil((Math.random() * 11) + 1);
+    crystalFour = numberOptions;
+    $("#four").removeClass("classFour");
+    $("#four").addClass("newCrystalFour");
+    crystalValue += crystalFour;
+    console.log(crystalFour);
+    console.log(crystalValue);
+    
+    
+});
+
+// Allowing crystal four to add to the running total by adding the same number after the first click.
+$(document).on("click", ".newCrystalFour", function () {
+    crystalValue += crystalFour;
+    console.log(crystalFour);
+    console.log(crystalValue);
+});
+
+
+// Conditional that identifies whether the user has hit the target number and wins, or goes over the target number and loses
+if (crystalValue === targetNumber) {
+    alert("You win!!");
+    wins++;
+}
+
+else if (crystalValue > targetNumber) {
+    alert("You lose!!!");
+    losses++;
+};
